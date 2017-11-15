d405e42d8e5bda4ba1dd6943dbcb7b07ac2551f6
Yugandhara Lad <yugandharalad9@gmail.com>
2017-11-15 13:23:08 -0500

    keyboard frame shifted Nov 15 122pm

--- Meme_2.0_Draft2/MemeEditorViewController.swift
+++ Meme_2.0_Draft2/MemeEditorViewController.swift
@@ -29,17 +29,8 @@ class MemeEditorViewController: UIViewController {
     
     
     
+    
    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     @IBAction func btnCancelClicked(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
